fc4be5a345af7383aa9044b5832a54c7cec2de9f
Alex Johnstone <alexjj@gmail.com>
2015-10-16 22:47:42 -0700

    org mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,4 +1,22 @@
+;; -*- emacs-lisp -*-
 ;;; alexjj emacs config
+
+;; org-mode
+
+(unless package-archive-contents    ;; Refresh the packages descriptions
+  (package-refresh-contents))
+(setq package-load-list '(all))     ;; List of packages to load
+(unless (package-installed-p 'org)  ;; Make sure the Org package is
+  (package-install 'org))           ;; installed, install it if not
+(package-initialize)                ;; Initialize & Install Package
+
+;;; Ledger
+
+(autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
+(add-to-list 'load-path
+             (expand-file-name "/usr/share/emacs/site-lisp/ledger-mode/"))
+(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+
 
 ;;; emacs
 
@@ -8,11 +26,12 @@
 (setq show-paren-delay 0)
 
 ;Interactively Do Things
+
 (require 'ido)
 (ido-mode t)
+(ido-everywhere)
+(setq ido-enable-flex-matching t)
+(setq ido-ignore-extensions t)
+(add-to-list 'completion-ignored-extensions ".pyc")
 
-;;; Ledger
-(autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
-(add-to-list 'load-path
-             (expand-file-name "/usr/share/emacs/site-lisp/ledger-mode/"))
-(add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+
