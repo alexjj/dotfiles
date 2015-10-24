3bd57ed10ddede50317ba115248580b62dacc491
Alex Johnstone <alexjj@gmail.com>
2015-10-24 13:17:05 -0700

    added org mode and zenburn theme

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -41,3 +41,33 @@
 
 ;; Use ibuffer
 (global-set-key (kbd "C-x C-b") 'ibuffer)
+
+
+;; flycheck
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; org
+
+     (global-set-key "\C-cl" 'org-store-link)
+     (global-set-key "\C-ca" 'org-agenda)
+     (global-set-key "\C-cc" 'org-capture)
+     (global-set-key "\C-cb" 'org-iswitchb)
+
+;; Zenburn theme
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes (quote (zenburn)))
+ '(custom-safe-themes
+   (quote
+    ("ff9e6deb9cfc908381c1267f407b8830bcad6028231a5f736246b9fc65e92b44" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
