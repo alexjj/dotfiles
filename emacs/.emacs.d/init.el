b9364eeb9e261f6fa73297be4039fe6722a46e59
Alex Johnstone <alexjj@gmail.com>
2016-01-14 18:21:38 -0800

    Started fresh with emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,79 +1,67 @@
 ;; -*- emacs-lisp -*-
 ;;; alexjj emacs config
 
-;; MELPA
-(require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
+;; INSTALL PACKAGES
+
+(require 'package)
+
+(add-to-list 'package-archives
+       '("melpa" . "http://melpa.org/packages/") t)
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar myPackages
+  '(better-defaults
+    material-theme
+    ledger-mode
+    elpy
+    flycheck
+    py-autopep8
+    ein
+    neotree))
+
+(mapc #'(lambda (package)
+    (unless (package-installed-p package)
+      (package-install package)))
+      myPackages)
+
+;; BASIC CUSTOMIZATION
+
+;(setq inhibit-startup-message t) ;; hide the startup message
+(load-theme 'material t) ;; load material theme
+(global-linum-mode t) ;; enable line numbers globally
+
+;;Auto backups
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq backup-by-copying t)
+(setq delete-old-versions t
+  kept-new-versions 6
+  kept-old-versions 2
+  version-control t)
 
 ;; Ledger
 
 (autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
 (add-to-list 'load-path
-             (expand-file-name "~/.emacs.d/elpa/ledger-mode-3.1/"))
+             (expand-file-name "~/.emacs.d/elpa/ledger-mode-20160111.1834/"))
 (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
 (setq ledger-post-auto-adjust-amounts t)
 
-;; beancount
-(add-to-list 'load-path "~/.emacs.d")
-(require 'beancount)
-(add-to-list 'auto-mode-alist '("\\.beancount\\'" . beancount-mode))
+;; Python Dev
 
-;; emacs
+(elpy-enable)
+(elpy-use-ipython)
 
-;; Show matching parentheses
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
 
-(show-paren-mode 1)
-(setq show-paren-delay 0)
+(require 'py-autopep8)
+(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
-;; line numbering
-(global-linum-mode 1)
-
-;; Auto indent with RET
-(define-key global-map (kbd "RET") 'newline-and-indent)
-
-;; Interactively Do Things
-
-(require 'ido)
-(ido-mode t)
-(ido-everywhere)
-(setq ido-enable-flex-matching t)
-(setq ido-ignore-extensions t)
-(add-to-list 'completion-ignored-extensions ".pyc")
-
-
-;; Use ibuffer
-(global-set-key (kbd "C-x C-b") 'ibuffer)
-
-
-;; flycheck
-(add-hook 'after-init-hook #'global-flycheck-mode)
-
-;; org
-
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cc" 'org-capture)
-(global-set-key "\C-cb" 'org-iswitchb)
-(setq org-agenda-include-diary t)
-
-;; Zenburn theme
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (zenburn)))
- '(custom-safe-themes
-   (quote
-    ("ff9e6deb9cfc908381c1267f407b8830bcad6028231a5f736246b9fc65e92b44" default))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-(put 'downcase-region 'disabled nil)
+;;neotree
+(setq neo-smart-open t)
+(global-set-key [f8] 'neotree-toggle)
