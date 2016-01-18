aa9451e04f97716aa0a75b14a668de8569c5ab44
Alex Johnstone <alexjj@gmail.com>
2016-01-17 21:47:05 -0800

    Updated org

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -21,7 +21,8 @@
     flycheck
     py-autopep8
     ein
-    neotree))
+    neotree
+    magit))
 
 (mapc #'(lambda (package)
     (unless (package-installed-p package)
@@ -33,6 +34,8 @@
 ;(setq inhibit-startup-message t) ;; hide the startup message
 (load-theme 'material t) ;; load material theme
 (global-linum-mode t) ;; enable line numbers globally
+(setq user-full-name "Alex Johnstone")
+(setq user-mail-address "alexjj@gmail.com")
 
 ;;Auto backups
 (setq backup-directory-alist `(("." . "~/.saves")))
@@ -54,6 +57,7 @@
 
 (elpy-enable)
 (elpy-use-ipython)
+(defalias 'workon 'pyvenv-workon)
 
 (when (require 'flycheck nil t)
   (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
@@ -62,6 +66,29 @@
 (require 'py-autopep8)
 (add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
 
+;;;magit
+(global-set-key (kbd "C-x g") 'magit-status)
+(global-set-key (kbd "C-x M-g") 'magit-dispatch-popup)
+
 ;;neotree
 (setq neo-smart-open t)
 (global-set-key [f8] 'neotree-toggle)
+
+;;org-mode configuration
+;; Enable org-mode
+(require 'org)
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+;; Set to the location of your Org files on your local system
+(setq org-directory "~/org")
+;; Set to the name of the file where new notes will be stored
+(setq org-mobile-inbox-for-pull "~/org/flagged.org")
+;; Set to <your Dropbox root directory>/MobileOrg.
+(setq org-mobile-directory "~/Dropbox/Apps/MobileOrg")
+(setq org-agenda-files (list "~/org/work.org"
+                             "~/org/python.org" 
+                             "~/org/home.org"))
+
+
