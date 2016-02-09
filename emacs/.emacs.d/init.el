967d8adb082cca6ad0d047a14d05679880876cc2
Alex Johnstone <alexjj@gmail.com>
2016-02-08 21:53:12 -0800

    Added spacemacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -97,3 +97,23 @@
                              "~/org/home.org"))
 
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ledger-reports
+   (quote
+    (("assetallocation" "ledger bal --percent AssetAllocation")
+     ("bal" "ledger -f %(ledger-file) bal")
+     ("reg" "ledger -f %(ledger-file) reg")
+     ("payee" "ledger -f %(ledger-file) reg @%(payee)")
+     ("account" "ledger -f %(ledger-file) reg %(account)")
+     ("monthlyexp" "ledger reg -M ^Expenses ^Income")
+     ("networth" "ledger bal assets liabilities -X GBP")))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
